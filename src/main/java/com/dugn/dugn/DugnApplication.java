b63b2d84dafdd591cdba9188d05d6faa7349ae84
dugn <dugn@tanmer.com>
2020-04-11 17:38:01 +0800

    新建spring项目

---
+++ src/main/java/com/dugn/dugn/DugnApplication.java
@@ -0,0 +1,13 @@
+package com.dugn.dugn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DugnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DugnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dugn/dugn/DugnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dugn.dugn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DugnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
